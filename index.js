fbba4caa777215438539ca261f48a48d94f8e0a3
voqse <git@voqse.com>
2022-01-12 02:05:44 +0300

    Empty express server and daemonize it

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('App running great!');
+import express from 'express';
+
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send('Hello from Node.js app running on VPS!');
+});
+
+server.listen(8080, () => {
+  console.log('Server running on http://localhost:8080');
+});
